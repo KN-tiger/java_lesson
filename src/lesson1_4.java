ea5f2d604ec6540b88bd49f8702ac7f1e66a3382
Kanayo Noguchi <krt10105@gmail.com>
2022-12-05 20:43:24 +0900

    基礎復習1

---
+++ src/lesson1_4.java
@@ -0,0 +1,12 @@
+public class lesson1_4 {
+    public static void main(String[] args) {
+        final double pi = 3.14;
+        int pie = 5;
+        System.out.println("半径" + pie + "cmのﾊﾟｲの面積は、");
+        System.out.println(pie * pie * pi);
+        System.out.println("パイの半径を倍にします");
+        pie = 10;
+        System.out.println("半径" + pie + "cmのﾊﾟｲの面積は、");
+        System.out.println(pie * pie * pi);
+    }
+}
