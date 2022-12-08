f791b3492ae9f4bb74a2b97785b097b36aba6007
Kanayo Noguchi <krt10105@gmail.com>
2022-12-08 20:39:49 +0900

    基礎復習2-2

--- src/lesson2_3.java
+++ src/lesson2_3.java
@@ -6,12 +6,22 @@ public class lesson2_3 {
         System.out.println("あなたの年齢を入力してください");
         String ageString = new java.util.Scanner(System.in).nextLine();
         int age = Integer.parseInt(ageString);
-        int fortune = new java.util.Random().nextInt(4);
-        fortune++;
         System.out.println("占いの結果が出ました！");
-        System.out.println
-                (age + "歳の" + name + "さん,あなたの運気番号は" + fortune +
-                "です");
-        System.out.println("1:大吉　2：中吉　3：吉　4：凶");
+        System.out.println( age + "歳の" + name + "さん");
+        int fortune = new java.util.Random().nextInt(4) + 1;
+        switch (fortune) {
+            case 1:
+                System.out.println("あなたの運気は大吉です！！");
+                break;
+            case 2:
+                System.out.println("あなたの運気は中吉です！");
+                break;
+            case 3:
+                System.out.println("あなたの運気は吉です");
+                break;
+            case 4:
+                System.out.println("あなたの運気は凶です…");
+                break;
+        }
     }
 }
